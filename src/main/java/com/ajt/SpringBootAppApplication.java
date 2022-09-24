739e892b45d41e4ffb8b9688ade2378adf4a1253
AjeetDubey <ajeetdubey92@gmail.com>
2022-09-24 22:51:21 +0530

    add init

--- src/main/java/com/ajt/SpringBootAppApplication.java
+++ src/main/java/com/ajt/SpringBootAppApplication.java
@@ -1,13 +1,21 @@
 package com.ajt;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootAppApplication {
+	
+	@PostConstruct
+	public void init() {
+		System.out.println("Initializinf resources..");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootAppApplication.class, args);
+		System.out.println("Application started.");
 	}
 
 }
